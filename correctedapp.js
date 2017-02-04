386befebf31044036a525ed741d5378eeec15ec9
camkirk <camden.kirkland@gmail.com>
2017-02-04 00:26:45 -0600

    added correctedapp.js

---
+++ correctedapp.js
@@ -0,0 +1,62 @@
+var mysql = require('mysql');
+var express = require('express');
+var path = require('path');
+var key = require('./keys.js')
+var app = express();
+app.use(express.static('public'))
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: key.pw,
+  database: "injection_db",
+  //multipleStatements: true
+});
+
+connection.connect(function(err) {
+  if (err) {throw err};
+});
+
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname, "public/html/view.html"));
+	//res.send('sup');
+
+});
+
+app.get('/query/',function(req,res){
+  var uid=req.query.ID;
+  var pass=req.query.pass;
+
+  var query = 'SELECT * FROM `accounts` WHERE `id`= ?';
+  console.log(query);
+  connection.query(query,uid, function(err, queryres) {
+		if (err) {throw err}
+		console.log(queryres);
+
+		res.json(queryres);
+
+
+	});
+});
+
+app.get('/query2/',function(req,res){
+  var uid=req.query.ID;
+  var pass=req.query.pass;
+
+  var query = 'SELECT * FROM `accounts` WHERE `id`= ? AND  `password`= ? ';
+
+  console.log(query);
+  connection.query(query,[uid,pass], function(err, queryres) {
+		if (err) {throw err}
+		console.log(queryres);
+
+		res.json(queryres);
+
+
+	});
+});
+
+app.listen(3000||process.env.port, function(){
+	console.log('listening on port')
+});
